a3f857a28647ca2754181b3db2df72486c795a72
KOMMEHTATOP <bahtiyarbasharov@gmail.com>
2020-11-16 21:40:10 +0300

    sfsfvsfg

--- udemy/mecharikov/js/dz/pereborMassiveDZ.js
+++ udemy/mecharikov/js/dz/pereborMassiveDZ.js
@@ -32,10 +32,36 @@ console.log(strArrBoolean) */
 строку, основываясь на index каждой буквы. Например:
 [{char:"H",index:0}, {char:"i",index: 1}, {char:"!",index:2}] → “Hi!” */
 
+/* const objArr = [
+    {char:"a",index:12}, 
+    {char:"w",index:8}, 
+    {char:"Y",index:10}, 
+    {char:"p",index:3}, 
+    {char:"p",index:2}, 
+    {char:"N",index:6}, 
+    {char:" ",index:5}, 
+    {char:"y",index:4}, 
+    {char:"r",index:13}, 
+    {char:"H",index:0}, 
+    {char:"e",index:11}, 
+    {char:"a",index:1}, 
+    {char:" ",index:9}, 
+    {char:"!",index:14}, 
+    {char:"e",index:7}
+]
+
+const poryadorObj = objArr.sort((prevChar, nextChar) => prevChar.index - nextChar.index).reduce((acc, currenValue) => acc += currenValue.char, '') 
+console.log(poryadorObj) */
+
 
 // -------------------------------Задание 5------------------------------------------
-/* Отсортируйте массив массивов так, чтобы вначале располагались наименьшие массивы (размер массива определяется его длиной): [ [14, 45], [1], ['a', 'c', 'd'] ] → [ [1], [14, 45], ['a', 'c', 'd'] ]
+/* Отсортируйте массив массивов так, чтобы вначале располагались наименьшие массивы (размер массива определяется его длиной): [ [14, 45], [1], ['a', 'c', 'd'] ] → [ [1], [14, 45], ['a', 'c', 'd'] ]*/
 
+/* const arrArrow = [ [14, 45], [1], ['a', 'c', 'd'] ]
+const newArrow = arrArrow.sort((prev, next) => prev.length - next.length)
+console.log(newArrow) */
+
+/*
 // -------------------------------Задание 6------------------------------------------
 Есть массив объектов:
 [
@@ -48,6 +74,17 @@ console.log(strArrBoolean) */
 
 Отсортировать их по возрастающему количеству ядер (cores). */
 
+/* const pc = [
+    {cpu: 'intel', info: {cores:2, сache: 3}},
+    {cpu: 'intel', info: {cores:4, сache: 4}},
+    {cpu: 'amd', info: {cores:1, сache: 1}},
+    {cpu: 'intel', info: {cores:3, сache: 2}},
+    {cpu: 'amd', info: {cores:4, сache: 2}}
+];
+const pcPor = pc.sort((prev, next) => prev.info.cores - next.info.cores)
+console.log(pcPor) */
+
+
 // -------------------------------Задание 7------------------------------------------
 /* 3. Создать функцию, которая будет принимать массив продуктов и две цены. Функция должна вернуть все продукты, цена которых находится в указанном диапазоне, и сортировать от дешевых к дорогим:
 
@@ -59,3 +96,19 @@ let products = [
 ];
 
 filterCollection(products, 15, 30) → [{...price: 15}, {...price: 18.9}, {...price: 19}, {...price: 25}] */
+
+/* let products = [
+    {title: 'prod1', price: 5.2}, {title: 'prod2', price: 0.18},
+    {title: 'prod3', price: 15}, {title: 'prod4', price: 25},
+    {title: 'prod5', price: 18.9}, {title: 'prod6', price: 8},
+    {title: 'prod7', price: 19}, {title: 'prod8', price: 63}
+    ]
+
+const filterCollection = (arr, minPrice, maxPrice) => {
+    const filterProd = arr.filter((item) => {
+        return item.price >= minPrice && item.price <= maxPrice
+    })
+    return filterProd.sort((prev, next) => prev.price - next.price)
+}
+
+console.log(filterCollection(products, 15, 19)) */

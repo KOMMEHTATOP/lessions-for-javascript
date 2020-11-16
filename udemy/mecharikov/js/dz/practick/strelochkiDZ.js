c4a6184453953edc91adb887a6098b33aad9f45c
KOMMEHTATOP <bahtiyarbasharov@gmail.com>
2020-11-16 16:56:18 +0300

    Perebor Massivov

--- udemy/mecharikov/js/dz/practick/strelochkiDZ.js
+++ udemy/mecharikov/js/dz/practick/strelochkiDZ.js
@@ -11,17 +11,17 @@ sum(1, 2, 3, 4); // 10
 sum(); // 0
 что такое метод reduce можно прочитать https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce*/
 
+/* const sum = (...params) => {
+  if (!params.length) return 0;
+  return params.reduce((prev, next) => prev + next);
+} */
 
-/* Переделать функцию с использованием функции-стрелки
+// Переделать функцию с использованием функции-стрелки
 
-function convertToObject(num) {
+/* function convertToObject(num) {
   const obj = {
       value: num,
       isOdd: Boolean(num % 2)
   }
   return obj;
 } */
-/* const convertToObject = (num) => ({
-  value: num,
-  isOdd: Boolean(num % 2)
-}) */
